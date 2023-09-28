2c391e9256493bb3f2b7ca60ab4852c3b1f0ab94
Sachin Kumar Payasi <109428604+SachinPayasi@users.noreply.github.com>
2023-09-28 21:05:39 +0530

    Update Assignment5.java

--- Alpha Plus/Assignment5.java
+++ Alpha Plus/Assignment5.java
@@ -89,10 +89,13 @@ public static void main(String args[]){
          for (int  i = 0; i  <= 5 ; i++ ) {
             System.out.println("i = "+i);
             }
+             System.out.println("i after the loop = "+ i );
         }
-         
-        
-    } 
+
+ /*Output:- error: cannot find symbol
+           System.out.println("i after the loop = "+ i );*/
+                                                     ^    
+} 
 
 
 
